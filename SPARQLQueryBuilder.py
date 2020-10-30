420b93cc7f5cd8b5dba19df5114833595da05719
Panos Mitzias <pmitzias@gmail.com>
2020-10-30 14:56:14 +0200

    First commit

---
+++ SPARQLQueryBuilder.py
@@ -0,0 +1,534 @@
+"""
+SPARQL Burger - A Python SPARQL query builder for programmatically generating SPARQL graph patterns and queries..
+Version 0.1
+Official webpage: http://pmitzias.com/SPARQL_Query_Builder
+Documentation: http://pmitzias.com/SPARQL_Query_Builder/docs.html
+Created by Panos Mitzias (http://www.pmitzias.com), Efstratios Kontopoulos (http://www.stratoskontopoulos.com)
+Powered by Catalink Ltd (http://catalink.eu)
+"""
+
+
+from SPARQLSyntaxTerms import *
+
+
+class SPARQLGraphPattern:
+    def __init__(self, optional=False, union=False):
+        """
+        The SPARQLGraphPattern class constructor.
+        :param optional: <bool> Indicates if graph pattern should be marked as OPTIONAL.
+        :param union: <bool> Indicates if graph pattern should have a UNION clause that associates it with the previous.
+        graph pattern
+        """
+        self.is_optional = optional
+        self.is_union = union
+        self.graph = []
+        self.filters = []
+        self.bindings = []
+
+    def add_triples(self, triples):
+        """
+        Adds a list of triples to the graph pattern.
+        :param triples: <list> A list of SPARQLSyntaxTerms.Triple objects.
+        :return: <bool> True if addition succeeded, False if given argument was not a list of Triple objects.
+        """
+        if type(triples) is list and all(isinstance(element, Triple) for element in triples):
+            self.graph.extend(triples)
+            return True
+        else:
+            return False
+
+    def add_nested_graph_pattern(self, graph_pattern):
+        """
+        Adds another graph pattern as nested to the main graph pattern.
+        :param graph_pattern: <obj> The SPARQLGraphPattern object to be nested.
+        :return: <bool> True if addition succeeded, False if given argument was not a SPARQLGraphPattern object.
+        """
+        if type(graph_pattern) is SPARQLGraphPattern:
+            self.graph.append(graph_pattern)
+            return True
+        else:
+            return False
+
+    def add_nested_select_query(self, select_query):
+        """
+        Adds a select query as nested to the main graph pattern.
+        :param select_query: <obj> The SPARQLSelectQuery object to be nested.
+        :return: <bool> True if addition succeeded, False if given argument was not a SPARQLGraphPattern object.
+        """
+        if type(select_query) is SPARQLSelectQuery:
+            self.graph.append(select_query)
+            return True
+        else:
+            return False
+
+    def add_filter(self, filter):
+        """
+        Adds a FILTER expression to the graph pattern.
+        :param filter: <obj> The Filter to be added.
+        :return: <bool> True if addition succeeded, False if given argument was not a Filter object.
+        """
+        if type(filter) is Filter:
+            self.filters.append(filter)
+            return True
+        else:
+            return False
+
+    def add_binding(self, binding):
+        """
+        Adds a BIND expression to the graph pattern.
+        :param binding: <obj> The Binding object to be added.
+        :return: <bool> True if addition succeeded, False if given argument was not a Binding object.
+        """
+        if type(binding) is Binding:
+            self.bindings.append(binding)
+            return True
+        else:
+            return False
+
+    def get_text(self, indentation_depth=0):
+        """
+        Generates the text for the SPARQL graph pattern.
+        :param indentation_depth: <int> A value that facilitates the appropriate addition of indents to the text. Defaults at 0.
+        :return: <str> The SPARQL graph pattern text. Returns empty string if an exception was raised.
+        """
+        try:
+            # Calculate indentations
+            outer_indentation = indentation_depth * "   "
+            inner_indentation = (indentation_depth + 1) * "   "
+
+            # Initialize string
+            if self.is_optional:
+                query_text = "%sOPTIONAL {\n" % (outer_indentation, )
+            elif self.is_union:
+                query_text = "%sUNION\n%s{\n" % (outer_indentation, outer_indentation)
+            else:
+                query_text = "%s{\n" % (outer_indentation, )
+
+            # Add triples
+            for entry in self.graph:
+                # If entry is a Triple object
+                if type(entry) is Triple:
+                    query_text += "%s%s" % (inner_indentation, entry.get_text())
+
+                # If entry is a nested SPARQLGraphPattern object
+                elif type(entry) is SPARQLGraphPattern:
+
+                    # Get text for nested graph pattern
+                    nested_graph_text = entry.get_text(indentation_depth=indentation_depth + 1)
+
+                    # Append nested text to graph text
+                    if nested_graph_text:
+                        query_text += nested_graph_text
+                    else:
+                        return False
+
+                # If entry is a nested SPARQLSelectQuery object
+                elif type(entry) is SPARQLSelectQuery:
+
+                    # Get the text for the nested select query
+                    nested_select_text = entry.get_text(indentation_depth=indentation_depth + 2)
+
+                    # Append nested text to graph text
+                    if nested_select_text:
+                        query_text += "%s{%s%s}\n" % (inner_indentation, nested_select_text, inner_indentation)
+                    else:
+                        return False
+
+            # Add binding texts
+            for binding in self.bindings:
+                query_text += "%s%s\n" % (inner_indentation, binding.get_text())
+
+            # Add filter texts
+            for filter in self.filters:
+                query_text += "%s%s\n" % (inner_indentation, filter.get_text())
+
+            # Finalize graph text
+            query_text += "%s}\n" % (outer_indentation, )
+
+            return query_text
+
+        except Exception as e:
+            print("Error 1 @ SPARQLGraphPattern.get_text()", e)
+            return ""
+
+
+class SPARQLQuery:
+    def __init__(self):
+        """
+        The SPARQLQuery class constructor.
+        """
+        self.prefixes = []
+        self.where = None
+
+    def add_prefix(self, prefix):
+        """
+        Adds a PREFIX expression to the graph pattern.
+        :param prefix: <obj> The Prefix object to be added.
+        :return: <bool> True if addition succeeded, False if given argument was not a Prefix object.
+        """
+        if type(prefix) is Prefix:
+            self.prefixes.append(prefix)
+            return True
+        else:
+            return False
+
+    def set_where_pattern(self, graph_pattern):
+        """
+        Sets the SPARQLGraphPattern object to be used at the WHERE part
+        :param graph_pattern: <obj> The SPARQLGraphPattern object to be used.
+        :return: <bool> True if setting succeeded, False if given argument was not a SPARQLGraphPattern object.
+        """
+        if type(graph_pattern) is SPARQLGraphPattern:
+            self.where = graph_pattern
+            return True
+        else:
+            return False
+
+
+class SPARQLSelectQuery(SPARQLQuery):
+    def __init__(self, distinct=False, limit=False):
+        """
+        The SPARQLSelectQuery class constructor.
+        :param distinct: <bool> Indicates if the select should be SELECT DISTINCT.
+        :param limit: <int> A limit to be used for the select query results.
+        """
+        SPARQLQuery.__init__(self)
+
+        self.distinct = distinct
+        self.limit = limit
+        self.variables = []
+        self.group_by = []
+
+    def add_variables(self, variables):
+        """
+        Adds a list of variables to be selected by the select query
+        :param variables: <list> A list of variables as strings.
+        :return: <bool> True if addition succeeded, False if given argument was not a list of strings.
+        """
+        if type(variables) is list and all(isinstance(element, str) for element in variables):
+            self.variables.extend(variables)
+            return True
+        else:
+            return False
+
+    def add_group_by(self, group):
+        """
+        Adds a GROUP BY expression to the query
+        :param group: <obj> The GroupBy object to be added
+        :return: <bool> True if addition succeeded, False if given argument was not a GroupBy object.
+        """
+        if type(group) is GroupBy:
+            self.group_by.append(group)
+            return True
+        else:
+            return False
+
+    def get_text(self, indentation_depth=0):
+        """
+        Generates the text for the SPARQL select query.
+        :param indentation_depth: <int> A value that facilitates the appropriate addition of indents to the text. Defaults at 0.
+        :return: <str> The SPARQL Select query text. Returns empty string if an exception was raised.
+        """
+        try:
+            # Calculate indentation
+            outer_indentation = indentation_depth * "   "
+
+            # Initialize text string
+            query_text = ""
+
+            # Add prefixes
+            for prefix in self.prefixes:
+                query_text += prefix.get_text()
+
+            # Add SELECT token
+            if self.distinct:
+                distinct_token = "DISTINCT "
+            else:
+                distinct_token = ""
+            query_text += "\n%sSELECT %s" % (outer_indentation, distinct_token)
+
+            # If some variables have been defined, add them
+            if self.variables:
+                query_text += " ".join(self.variables)
+
+            # If no variable has been defined, use *
+            else:
+                query_text += " *"
+
+            # Add WHERE token
+            query_text += "\n%sWHERE " % (outer_indentation, )
+
+            # Add WHERE pattern graph
+            query_text += self.where.get_text(indentation_depth=indentation_depth)[:-1]
+
+            # Add group by expressions
+            for group in self.group_by:
+                query_text += "\n%s%s" % (outer_indentation, group.get_text())
+
+            # Add limit if required
+            if self.limit:
+                query_text += "\nLIMIT %s" % (str(self.limit))
+
+            return query_text
+
+        except Exception as e:
+            print("Error 1 @ SPARQLSelectQuery.get_text()", e)
+            return ""
+
+
+class SPARQLUpdateQuery(SPARQLQuery):
+    def __init__(self):
+        """
+        The SPARQLUpdateQuery class constructor.
+        """
+        SPARQLQuery.__init__(self)
+        self.delete = None
+        self.insert = None
+
+    def set_delete_pattern(self, graph_pattern):
+        """
+        Sets the SPARQLGraphPattern object to be used at the DELETE part
+        :param graph_pattern: <obj> The SPARQLGraphPattern object to be used.
+        :return: <bool> True if setting succeeded, False if given argument was not a SPARQLGraphPattern object.
+        """
+        if type(graph_pattern) is SPARQLGraphPattern:
+            self.delete = graph_pattern
+            return True
+        else:
+            return False
+
+    def set_insert_pattern(self, graph_pattern):
+        """
+        Sets the SPARQLGraphPattern object to be used at the INSERT part.
+        :param graph_pattern: <obj> The SPARQLGraphPattern object to be used.
+        :return: <bool> True if setting succeeded, False if given argument was not a SPARQLGraphPattern object.
+        """
+        if type(graph_pattern) is SPARQLGraphPattern:
+            self.insert = graph_pattern
+            return True
+        else:
+            return False
+
+    def get_text(self, indentation_depth=0):
+        """
+        Generates the text for the SPARQL update query.
+        :param indentation_depth: <int> A value that facilitates the appropriate addition of indents to the text. Defaults at 0.
+        :return: <str> The SPARQL Update query text. Returns empty string if an exception was raised.
+        """
+
+        try:
+            # Calculate indentation
+            outer_indentation = indentation_depth * "   "
+
+            # Initialize text string
+            query_text = ""
+
+            # Add prefixes
+            for prefix in self.prefixes:
+                query_text += prefix.get_text()
+
+            # If a delete graph pattern has been defined
+            if self.delete is not None:
+
+                # Add DELETE token
+                query_text += "\n%sDELETE " % (outer_indentation,)
+
+                # Add DELETE pattern graph
+                query_text += self.delete.get_text(indentation_depth=indentation_depth)[:-1]
+
+            # If an insert graph pattern has been defined
+            if self.insert is not None:
+                # Add INSERT token
+                query_text += "\n%sINSERT " % (outer_indentation,)
+
+                # Add INSERT pattern graph
+                query_text += self.insert.get_text(indentation_depth=indentation_depth)[:-1]
+
+            # If a where graph pattern has been defined
+            if self.where is not None:
+                # Add WHERE token
+                query_text += "\n%sWHERE " % (outer_indentation,)
+
+                # Add WHERE pattern graph
+                query_text += self.where.get_text(indentation_depth=indentation_depth)[:-1]
+
+            return query_text
+
+        except Exception as e:
+            print("Error 1 @ SPARQLUpdateQuery.get_text()", e)
+            return ""
+
+
+if __name__ == "__main__":
+
+    # Create a graph pattern
+    pattern = SPARQLGraphPattern()
+
+    # Add a triple
+    pattern.add_triples(
+        triples=[Triple(
+            subject="?person",
+            predicate="rdf:type",
+            object="ex:Person"
+        )]
+    )
+
+    # Add a second triple
+    pattern.add_triples(
+        triples=[Triple(
+            subject="?person",
+            predicate="ex:hasName",
+            object="?name"
+        )]
+    )
+
+    # Create a second graph pattern that should be OPTIONAL
+    optional_pattern = SPARQLGraphPattern(optional=True)
+
+    # Add a triple to the optional graph pattern
+    optional_pattern.add_triples(
+        triples=[Triple(
+            subject="?person",
+            predicate="ex:hasAge",
+            object="?age"
+        )]
+    )
+
+    # Αδδ optional graph pattern as nested to the main graph pattern
+    pattern.add_nested_graph_pattern(optional_pattern)
+
+    # Add some binding (BIND clause) to the main pattern.
+    pattern.add_binding(
+        binding=Binding(
+            value="\'John\'@en",
+            variable="?name"
+        )
+    )
+
+    # Add another binding where we will use a nested IF clause with a BOUND clause
+    pattern.add_binding(
+        binding=Binding(
+            value=IfClause(
+                condition=Bound(
+                    variable="?age"
+                ),
+                true_value="?age",
+                false_value="32"
+            ),
+            variable="?years"
+        )
+    )
+
+    # Add a filter for the age to the optional pattern
+    optional_pattern.add_filter(
+        filter=Filter(
+            expression="?age > 30"
+        )
+    )
+
+    # Print the graph query text
+    print(pattern.get_text())
+
+    # Create a select query
+    select_query = SPARQLSelectQuery(
+        distinct=True,
+        limit=1000
+    )
+
+    # Add prefix "ex"
+    select_query.add_prefix(
+        prefix=Prefix(
+            prefix="ex",
+            namespace="http://www.example.com#"
+        )
+    )
+
+    # Add the variables for the selection
+    select_query.add_variables("?person")
+    select_query.add_variables("?years")
+
+    # Set the graph pattern that we created earlier as the WHERE pattern
+    select_query.set_where_pattern(graph_pattern=pattern)
+
+    # Add a GROUP BY to query
+    select_query.add_group_by(
+        group=GroupBy(
+            variables=["?person", "?age"]
+        )
+    )
+
+    # # Create another graph pattern
+    # pattern_3 = SPARQLGraphPattern()
+    # pattern_3.add_triple(
+    #     triple=Triple(
+    #         subject="?person",
+    #         predicate="rdf:type",
+    #         object="ex:Customer"
+    #     )
+    # )
+    #
+    # # Create a new select query to be nested in the first select query
+    # nested_select_query = SPARQLSelectQuery()
+    # nested_select_query.add_variable("?person")
+    # nested_select_query.set_where_pattern(
+    #     graph_pattern=pattern_3
+    # )
+    #
+    # # Set the second query as nested to the first graph pattern
+    # pattern.add_nested_select_query(
+    #     select_query=nested_select_query
+    # )
+
+    # Print the query text
+    print(select_query.get_text())
+
+    #########################################
+    # delete_pattern = SPARQLGraphPattern()
+    # delete_pattern.add_triple(
+    #     Triple(
+    #         subject="?person",
+    #         predicate="ex:hasAge",
+    #         object="?age"
+    #     )
+    # )
+    #
+    # insert_pattern = SPARQLGraphPattern()
+    # insert_pattern.add_triple(
+    #     Triple(
+    #         subject="?person",
+    #         predicate="ex:hasAge",
+    #         object="32"
+    #     )
+    # )
+    #
+    # where_pattern = SPARQLGraphPattern()
+    # where_pattern.add_triple(
+    #     Triple(
+    #         subject="?person",
+    #         predicate="ex:hasAge",
+    #         object="?age"
+    #     )
+    # )
+    # where_pattern.add_triple(
+    #     Triple(
+    #         subject="?person",
+    #         predicate="ex:name",
+    #         object="John"
+    #     )
+    # )
+    #
+    # # Create an update query
+    # update_query = SPARQLUpdateQuery()
+    # update_query.add_prefix(
+    #     Prefix(
+    #         prefix="ex",
+    #         namespace="http://www.example.com#"
+    #     )
+    # )
+    # update_query.set_delete_pattern(graph_pattern=delete_pattern)
+    # update_query.set_insert_pattern(graph_pattern=insert_pattern)
+    # update_query.set_where_pattern(graph_pattern=insert_pattern)
+    #
+    # print(update_query.get_text())
+
+
---
+++ SPARQLSyntaxTerms.py
@@ -0,0 +1,188 @@
+
+
+class Prefix:
+    def __init__(self, prefix, namespace):
+        """
+        The Prefix class constructor.
+        :param prefix: <str> The prefix (e.g. "ex").
+        :param namespace: <str> The namespace (e.g. "http://www.example.com#").
+        """
+        self.prefix = prefix
+        self.namespace = namespace
+
+    def get_text(self):
+        """
+        Generates the text for the given prefix (e.g. "PREFIX ex: <http://www.example.com#>")
+        :return: <str> The prefix definition text. Returns empty string if an exception was raised.
+        """
+        try:
+            return "PREFIX %s: <%s>\n" % (self.prefix, self.namespace)
+        except Exception as e:
+            print("Error 1 @ Prefix.get_text()")
+            return ""
+
+
+class Triple:
+    def __init__(self, subject, predicate, object):
+        """
+        The Triple class constructor.
+        :param subject: <str> The subject string (e.g. "?person")
+        :param predicate: <str> The predicate string (e.g. "ex:hasName")
+        :param object: <str> The object string (e.g. "\'John\'@en")
+        """
+        self.subject = str(subject)
+        self.predicate = str(predicate)
+        self.object = str(object)
+
+    def get_text(self):
+        """
+        Generates the text for the given triple.
+        :return: <str> The triple definition text. Returns empty string if an exception was raised.
+        """
+        try:
+            return "%s %s %s . \n" % (self.subject, self.predicate, self.object)
+        except Exception as e:
+            print("Error 1 @ Triple.get_text()")
+            return ""
+
+
+class Filter:
+    def __init__(self, expression):
+        """
+        The Filter class constructor.
+        :param expression: <str> The expression to get in the filter (e.g. "?age > 30")
+        """
+        self.expression = expression
+
+    def get_text(self):
+        """
+        Generates the text for the given filter.
+        :return: <str> The filter definition text. Returns empty string if an exception was raised.
+        """
+        try:
+            return "FILTER (%s)" % (self.expression,)
+        except Exception as e:
+            print("Error 1 @ Filter.get_text()")
+            return ""
+
+
+class Binding:
+    def __init__(self, value, variable):
+        """
+        The Binding class constructor.
+        :param value: <str> A string value to get in the BIND first part (e.g. "John")
+         OR <obj> Another object (e.g. IfClause) to be nested.
+        :param variable: <str> The variable to be bound to this value (e.g. "?name")
+        """
+        self.value = value
+        self.variable = variable
+
+    def get_text(self):
+        """
+        Generates the text for the given binding (e.g. "BIND('John' AS ?name)" or "BIND(IF(BOUND(...)) AS ?name") )
+        :return: <str> The binding definition text. Returns empty string if an exception was raised.
+        """
+        try:
+            if type(self.value) is str:
+                value_text = self.value
+            else:
+                value_text = self.value.get_text()
+
+            return "BIND (%s AS %s)" % (value_text, self.variable)
+
+        except Exception as e:
+            print("Error 1 @ Binding.get_text()")
+            return ""
+
+
+class Bound:
+    def __init__(self, variable):
+        """
+        The Binding class constructor.
+        :param variable: <str> The variable to be checked if it is bound (e.g. "?name")
+         OR <obj> Another object to be nested.
+        """
+        self.variable = variable
+
+    def get_text(self):
+        """
+        Generates the text for the given BOUND clause (e.g. "BOUND (?name)" )
+        :return: <str> The bound definition text. Returns empty string if an exception was raised.
+        """
+        try:
+            if type(self.variable) is str:
+                variable_text = self.variable
+            else:
+                variable_text = self.variable.get_text()
+
+            return "BOUND (%s)" % (variable_text, )
+
+        except Exception as e:
+            print("Error 1 @ Bound.get_text()")
+            return ""
+
+
+class IfClause:
+    def __init__(self, condition, true_value, false_value):
+        """
+        The Binding class constructor.
+        :param condition: <str> The condition for the IF clause OR <obj> Another object to be nested.
+        :param true_value: <str> The value for when IF condition is True OR <obj> Another object to be nested.
+        :param false_value: <str> The value for when IF condition is False OR <obj> Another object to be nested.
+        """
+        self.condition = condition
+        self.true_value = true_value
+        self.false_value = false_value
+
+    def get_text(self):
+        """
+        Generates the text for the given BOUND clause (e.g. "IF(?age > 18, 'adult', 'minor')" )
+        :return: <str> The if clause definition text. Returns empty string if an exception was raised.
+        """
+        try:
+            # Check for nested condition (e.g. a nested if condition)
+            if type(self.condition) is str:
+                condition_text = self.condition
+            else:
+                condition_text = self.condition.get_text()
+
+            # Check for nested value (e.g. a nested if value)
+            if type(self.true_value) is str:
+                true_value_text = self.true_value
+            else:
+                true_value_text = self.true_value.get_text()
+
+            # Check for nested value (e.g. a nested if value)
+            if type(self.false_value) is str:
+                false_value_text = self.false_value
+            else:
+                false_value_text = self.false_value.get_text()
+
+            return "IF (%s, %s, %s)" % (condition_text, true_value_text, false_value_text)
+
+        except Exception as e:
+            print("Error 1 @ IfClause.get_text()")
+            return ""
+
+
+class GroupBy:
+    def __init__(self, variables):
+        """
+        The GroupBy class constructor.
+        :param variables: <list> A list of variables as strings that will be used for the grouping
+        """
+        self.variables = variables
+
+    def get_text(self):
+        """
+        Generates the text for the given GROUP BY expression (e.g. "GROUP BY ?person ?age")
+        :return: <str> The GROUP BY definition text. Returns empty string if an exception was raised.
+        """
+        try:
+            return "GROUP BY %s" % (" ".join(self.variables), )
+
+        except Exception as e:
+            print("Error 1 @ GroupBy.get_text()")
+            return ""
+
+
